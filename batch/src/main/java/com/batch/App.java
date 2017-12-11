c0e924b7736a8f0ff26298ee6b2f439b75d227f1
xz <xz@192.168.56.1>
2017-12-11 23:15:55 +0800

    主服务启动

--- batch/src/main/java/com/batch/App.java
+++ batch/src/main/java/com/batch/App.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class App {
+	//启动主服务
     public static void main(String[] args) {
         SpringApplication.run(App.class,args);
     }
