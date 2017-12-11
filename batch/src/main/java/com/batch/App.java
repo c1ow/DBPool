87176e810fe589aba25a33c829571430cb5b856c
c1ow <1127366104@qq.com>
2017-12-11 23:24:53 +0800

    提交

--- batch/src/main/java/com/batch/App.java
+++ batch/src/main/java/com/batch/App.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class App {
-	//启动主服务sss
+	//启动主服务
     public static void main(String[] args) {
         SpringApplication.run(App.class,args);
     }
